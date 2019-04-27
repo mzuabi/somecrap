5f75b168ed6d4c21dcd1f9c5a6abccfecfcf8db0
mzuabi <50050503+mzuabi@users.noreply.github.com>
2019-04-27 10:48:28 -0400

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,5 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "My new Line"
 echo "Well its another try"
+echo "Well its another try for the not sure how many times"
 echo "this was run automagically"
