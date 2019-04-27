fdaf2076622ea95d162ea0b7c6a725d5ac5ea191
root <root@mzuabi1c.mylabserver.com>
2019-04-27 15:03:54 +0000

    new change

--- testscript.sh
+++ testscript.sh
@@ -4,7 +4,6 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "My new Line"
 echo "Well its another try"
-<<<<<<< HEAD
 echo "Well its another try for the not sure how many times"
 echo "this was run automagically"
-=======>>>>>>> 8a0a84e9017d99d67e5ac73edd6fa686c7288fbc
+echo "try again"
